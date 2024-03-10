29a3e2b43b0f5eff75d3f0c5a03b028bf7e33251
Damian Cannon <damianbcannon@hotmail.com>
2024-03-10 18:50:57 +0000

    Count letters in text file

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ def main():
         print(file_contents)
 
 def count_words():
-    # Open book and print to console
+    # Open book and count words
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         return len(file_contents.split())
+        
+def count_letters():
+    # Open book and count letters
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        lower_file_contents = file_contents.lower()
+
+        results = {}
+        for char in lower_file_contents:
+            if char in results:
+                results[char] += 1
+            else:
+                results[char] = 1
+
+        return results
 
 print(f"Number of words in document: {count_words()}")
+print(f"Number of letters in document: {count_letters()}")
